cc65cd631d0b7747991c822b93a0b8f1cc95af18
nome <email>
2025-07-21 05:52:54 -0300

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import './App.css'
+
+import Header from './components/header/header.jsx'
+
+function App() {
+  return (
+    <div>
+      <Header />
+      
+      {/* Add your page content here */}
+    </div>
+  )
+}
+
+export default App
