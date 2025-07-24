13671284c3ec4f7a4ee09ed7db97278eda917d6a
nome <email>
2025-07-24 03:46:46 -0300

    build-vercel

--- .next/static/chunks/app/layout-c2eff41431a7dd74.js
+++
@@ -1,1 +0,0 @@
-(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[177],{567:(e,s,l)=>{Promise.resolve().then(l.t.bind(l,9324,23)),Promise.resolve().then(l.t.bind(l,9840,23))},9324:()=>{},9840:e=>{e.exports={style:{fontFamily:"'Inter', 'Inter Fallback'",fontStyle:"normal"},className:"__className_e8ce0c"}}},e=>{e.O(0,[385,441,964,358],()=>e(e.s=567)),_N_E=e.O()}]);
